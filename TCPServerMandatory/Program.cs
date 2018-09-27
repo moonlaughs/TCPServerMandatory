3a331b8c6962760dbfb4ced27780a6ad36994cbf
Izabela Kuzniar <izab0583@edu.easj.dk>
2018-09-27 21:25:37 +0200

    Add project files.

---
+++ TCPServerMandatory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCPServerMandatory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
